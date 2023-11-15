bfd370bb313b399abdeb1a31ed52d01bae9de13a
mj-surya <mj7surya@gmail.com>
2023-11-15 18:47:57 +0530

    hotels model

--- HotelBookingSolution/HotelBookingApplication/Models/Hotel.cs
+++ HotelBookingSolution/HotelBookingApplication/Models/Hotel.cs
@@ -12,5 +12,6 @@ namespace HotelBookingApplication.Models
         public string Phone { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
+        public string Amenities { get; set; }
     }
 }
---
+++ HotelBookingSolution/HotelBookingApplication/Services/HotelService.cs
@@ -0,0 +1,70 @@
+﻿using HotelBookingApplication.Exceptions;
+using HotelBookingApplication.Interfaces;
+using HotelBookingApplication.Models;
+using HotelBookingApplication.Models.DTOs;
+
+namespace HotelBookingApplication.Services
+{
+    public class HotelService : IHotelService
+    {
+        private readonly IRepository<int, Hotel> _hotelRepository;
+        public HotelService(IRepository<int, Hotel> repository)
+        {
+            _hotelRepository = repository;
+        }
+
+        public HotelDTO AddHotel(HotelDTO hotelDTO)
+        {
+            Hotel hotel = new Hotel()
+            {
+                HotelName = hotelDTO.HotelName,
+                City = hotelDTO.City,
+                Address = hotelDTO.Address,
+                Phone = hotelDTO.Phone,
+                Image = hotelDTO.Image,
+                Description = hotelDTO.Description,
+                Amenities = hotelDTO.Amenities
+            };
+            var result = _hotelRepository.Add(hotel);
+            if(result != null)
+            {
+                return hotelDTO;
+            }
+            return null;
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            var hotels = _hotelRepository.GetAll();
+            if(hotels != null)
+            {
+                return hotels.ToList();
+            }
+            throw new NoHotelsAvailableException();
+        }
+
+        public bool RemoveHotel(int id)
+        {
+            var result = _hotelRepository.Delete(id);
+            return true;
+        }
+
+        public HotelDTO UpdateHotel(int id,HotelDTO hotelDTO)
+        { 
+            var hotel = _hotelRepository.GetById(id);
+            if(hotel != null)
+            {
+                hotel.Phone = hotelDTO.Phone;
+                hotel.Image = hotelDTO.Image;
+                hotel.Address = hotelDTO.Address;
+                hotel.HotelName = hotelDTO.HotelName;
+                hotel.City = hotelDTO.City;
+                hotel.Description = hotelDTO.Description;
+                hotel.Amenities = hotelDTO.Amenities;
+                var result = _hotelRepository.Update(hotel);
+                return hotelDTO;
+            }
+            return null;
+        }
+    }
+}
